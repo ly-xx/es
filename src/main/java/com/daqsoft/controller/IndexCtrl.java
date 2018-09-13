11b5158727a1c54c93a5db83ad62ea7cc67535c9
liaoxiaoxia <liaoxx@daqsoft.com.cn>
2018-09-13 19:26:22 +0800

    索引管理

--- src/main/java/com/daqsoft/controller/IndexCtrl.java
+++ src/main/java/com/daqsoft/controller/IndexCtrl.java
@@ -3,12 +3,11 @@ package com.daqsoft.controller;
 import com.daqsoft.commons.responseEntity.BaseResponse;
 import com.daqsoft.commons.responseEntity.ResponseBuilder;
 import com.daqsoft.utils.TransportClientUtil;
-import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
-import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequestBuilder;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
 import org.elasticsearch.action.admin.indices.exists.types.TypesExistsResponse;
 import org.elasticsearch.client.IndicesAdminClient;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,8 +37,31 @@ public class IndexCtrl {
                     .setTypes("type_news").get();
             System.out.println("类型是否存在：" + typesExistsResponse.isExists());
             // 创建索引
-            CreateIndexResponse createIndexResponse = adminClient.prepareCreate(index).get();
-            // 设置mapping.....
+            adminClient.prepareCreate(index).get();
+            // 创建mapping
+            adminClient.preparePutMapping(index.toLowerCase())
+                    .setType(index)
+                    .setSource(XContentFactory.jsonBuilder()
+                            .startObject()
+                            .startObject("properties")
+                            .startObject("name")
+                            .field("type", "string")
+                            .endObject()
+                            .endObject()
+                            .endObject())
+                    .get();
+            // 创建带mapping的索引
+//            CreateIndexResponse createIndexResponse = adminClient.prepareCreate(index.toLowerCase())
+//                    .addMapping(index, XContentFactory.jsonBuilder()
+//                            .startObject()
+//                            .startObject("properties")
+//                            .startObject("name")
+//                            .field("type", "string")
+//                            .endObject()
+//                            .endObject()
+//                            .endObject())
+//                    .get();
+            // 获取索引、删除索引......
             return ResponseBuilder.custom().success().build();
         } catch (Exception e) {
             e.printStackTrace();
